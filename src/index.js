db1b0d1429dfd75c035f71b777091acfa168a4d3
Charley DAVID <hello@calyh.re>
2019-05-02 17:22:08 -0300

    Fix wrong extend of original lib

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import json1 from 'ot-json1';
 import apply, { registerSubtype } from './apply';
 
-export default { ...json1, apply, registerSubtype };
+export default { ...json1, type: { ...json1.type, apply, registerSubtype } };
